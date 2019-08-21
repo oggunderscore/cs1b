d438d4b318c60bdc3e14a522cc21fe504f55ed8c
Felix Murray <felix.murray@gmail.com>
2019-08-21 07:24:26 -0700

    Added void functions for main menu, printing main menu, and cashier module.

--- project-serendipity/main.cpp
+++ project-serendipity/main.cpp
@@ -1,33 +1,86 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std; 
+
+void cashierModule (){
+  string date; 
+  int quantity; 
+  string ISBN; 
+  string title; 
+  float price; 
+
+  system("clear");
+
+  cout << "Serendipity Booksellers"
+       << "\nCashier Module\n\n";
+  cout << "Date: ";
+  cin.ignore();
+  getline (cin, date);
+  cout << "Quantity of Book: ";
+  cin.ignore();
+  cin >> quantity;
+  cout << "ISBN: ";
+  cin.ignore();
+  getline (cin, ISBN);
+  cout << "Title: ";
+  cin.ignore();
+  getline (cin, title);
+  cout << "Price: ";
+  cin >> price;
+
+  cout << 
+}
+
+void mainMenu (char userSelection){
+   //system("clear");
+   switch (userSelection){
+      case '1': 
+        cashierModule(); 
+        break; 
+      case '2': 
+        system("read");
+        cout << "You entered option 2. Nothing to see here yet!";
+        cout << "Press any key to continue...";
+        
+        break; 
+      case '3': 
+        cout << "You entered option 3."
+             << "\nNothing to see here yet!"; 
+        cout << "Press any key to continue...\n";
+        cin.ignore();
+        // system("read");
+        break; 
+      case '4':
+        userSelection = '4';
+        break; 
+    }
+}
+
+void printMainMenu () {
+    system("clear");
+    cout << "Serendipity Book Sellers"
+         << "\n" << "Main Menu" << "\n\n";
+    cout << "1.   Cashier Module"
+         << "\n"
+         << "2.   Inventory Database Module"
+         << "\n"
+         << "3.   Report Module"
+         << "\n"
+         << "4.   Exit"
+         << "\n\n"
+         << "Enter Your Choice: ";    
+  }
 
 int main () {
-    char menuOption; 
-
-    do{
-      std::cout << "SERENDIPITY BOOK SELLERS"
-              << "\n\n";
-    std::cin >> menuOption; 
-    } while (true);
+    char menuOption;
+    do {
+      printMainMenu();
+      cin >> menuOption;
+      mainMenu (menuOption);
+    } while (menuOption != '4');
     
 	return 0; 
 }
 
-void serendipityMenu (char userSelection){
-   switch (userSelection){
-      case '1': 
-        //something 
-        break; 
-      case '2': 
-        //something else
-        break; 
-      case '3': 
-        //something else 2
-        break; 
-      case '4';
-        //something else 3
-        break; 
-
-    }
-
-}
 
