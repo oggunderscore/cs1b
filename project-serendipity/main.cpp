6bc57c9873369f3bb9993698a872743f6158de31
Felix Murray <felix.murray@gmail.com>
2019-08-21 09:13:10 -0700

    Added more formatting to the menus and added Inventory print and use void functins.

--- project-serendipity/main.cpp
+++ project-serendipity/main.cpp
@@ -4,11 +4,14 @@
 using namespace std; 
 
 void cashierModule (){
+  const float SALES_TAX = 0.06; 
   string date; 
   int quantity; 
   string ISBN; 
   string title; 
   float price; 
+  float total;
+  float taxToAdd;
 
   system("clear");
 
@@ -18,7 +21,6 @@ void cashierModule (){
   cin.ignore();
   getline (cin, date);
   cout << "Quantity of Book: ";
-  cin.ignore();
   cin >> quantity;
   cout << "ISBN: ";
   cin.ignore();
@@ -29,20 +31,77 @@ void cashierModule (){
   cout << "Price: ";
   cin >> price;
 
-  cout << 
+  total = quantity * price; 
+  taxToAdd = total * SALES_TAX;
+
+  system("clear");
+  cout << "Serendipity Booksellers\n\n"; 
+  cout << "Date: " << date; 
+  cout << "\n\n";
+  cout << "Qty     ISBN          Title                    Price          Total\n";
+  cout << "_______________________________________________________________________\n";
+  cout << quantity << "     " << ISBN << "     " 
+       << title << "         $  " << price 
+       << "           $  " << total << "\n\n\n";
+  cout << "            Subtotal                         "
+       << total;
+  cout << "            Tax                         $ " << taxToAdd;
+  cout << "            Total                       $ " << total + taxToAdd; 
+  cout << "Thank you for shopping at Serendipity!";
+  system("read");
+}
+void printInventoryMenu () {
+    system("clear");
+    cout << "Serendipity Book Sellers"
+         << "\n" << "Inventory Database" << "\n\n";
+    cout << "1.   Look Up a Book"
+         << "\n"
+         << "2.   Add a Book"
+         << "\n"
+         << "3.   Edit a Book's Record"
+         << "\n"
+         << "4.   Delete a Book"
+         << "\n"
+         << "5.   Return to the Main Menu"
+         << "\n\n"
+         << "Enter Your Choice: ";    
 }
 
+void inventoryModule (char userSelection) {
+  do {
+    switch (userSelection){
+      case '1':
+        cout << "nothing here yet (1)";
+        break;
+      case '2':
+        cout << "nothing here yet (2)";
+        break;
+      case '3':
+        cout << "nothing here yet (3)";
+        break;
+      case '4':
+        cout << "nothing here yet (4)";
+        break;
+      case '5':
+        break;
+      default: 
+        cout << "ERROR: Input Must be an Option from 1 - 4.";
+        break;
+    }
+  } while (userSelection != '5');
+}
+
+
 void mainMenu (char userSelection){
-   //system("clear");
+   system("clear");
    switch (userSelection){
       case '1': 
         cashierModule(); 
         break; 
       case '2': 
-        system("read");
-        cout << "You entered option 2. Nothing to see here yet!";
-        cout << "Press any key to continue...";
-        
+        printInventoryMenu();
+        cin >> userSelection; 
+        inventoryModule(userSelection); 
         break; 
       case '3': 
         cout << "You entered option 3."
@@ -53,7 +112,10 @@ void mainMenu (char userSelection){
         break; 
       case '4':
         userSelection = '4';
-        break; 
+        break;
+      default: 
+        cout << "ERROR: Input Must be an Option from 1 - 4.";
+        break;  
     }
 }
 
