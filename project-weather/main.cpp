f8d645a7742d5948902d0730c6d060230dddf134
Felix <felix.murray@gmail.com>
2019-08-26 17:25:21 -0700

    [fix] Organize code for weather project.

--- project-weather/main.cpp
+++ project-weather/main.cpp
@@ -1,86 +1,107 @@
 #include <iostream>
-#define months 12
+#include <iomanip>
+#define TOTAL_MONTHS 12
 
 using namespace std;
 
 //functions: getData, averageHigh, averageLow, indexHighTemp, indexLowTemp
 
-void getData(double array[][months]);
-double averageHigh(double array[][months]);
-double averageLow(double array[][months]);
-int indexHighTemp(double array[][months]);
-int indexLowTemp(double array[][months]);
+const string DEGREE_SYMBOL = "\u00b0";
+const string MONTH_NAMES[TOTAL_MONTHS] = {
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+};
+
+void getData(double array[TOTAL_MONTHS][2]);
+void printMonth(int index);
+double averageHigh(double array[TOTAL_MONTHS][2]);
+double averageLow(double array[TOTAL_MONTHS][2]);
+int indexHighTemp(double array[TOTAL_MONTHS][2]);
+int indexLowTemp(double array[TOTAL_MONTHS][2]);
 
 int main () {
-    double temps[2][months];
+    double months[TOTAL_MONTHS][2];
 
-    getData(temps);
-    cout << "Yearly Statistics:/n/n";
-    cout << "\nAverage high temperature: ";
-    cout << averageHigh(temps);
-    cout << "\nAverage low temperature: ";
-    cout << averageLow(temps);
-    cout << "\nHighest temperature: ";
-    cout << indexHighTemp(temps);
-    cout << "\nLowest temperature: ";
-    cout << indexLowTemp(temps);
+    getData(months);
+    cout << "Yearly Statistics:\n\n";
+    cout << "Average high temperature: ";
+    cout << averageHigh(months) << DEGREE_SYMBOL << "\n";
+    cout << "Average low temperature: ";
+    cout << averageLow(months) << DEGREE_SYMBOL << "\n";
+    cout << "Highest temperature: ";
+    cout << indexHighTemp(months) << DEGREE_SYMBOL << "\n";
+    cout << "Lowest temperature: ";
+    cout << indexLowTemp(months) << DEGREE_SYMBOL << "\n";
 
     return 0;
 }
 
-//High Temperatures are stored in first row (index 0)
-//Low Temperatures are stored in second row (index 1)
-void getData(double array[][months]) {
-  for (int r = 0; r < 2; r++){
-    for (int c = 0; c < months; c++){
-        switch (r) {
-          case 0:
-            cout << "Enter High: ";
-            cin >> array[r][c];
-            cout << "\n";
-            break;
-          case 1:
-            cout << "Enter Low: ";
-            cin >> array[r][c];
-            cout << "\n";
-            break;
-        }
-    }
+void getData(double array[TOTAL_MONTHS][2]) {
+  for (int month = 0; month < TOTAL_MONTHS; month++) {
+    printMonth(month);
+    
+    cout << "-- Enter High: ";
+    cin >> array[month][0];
+    cout << "\n";
+
+    cout << "-- Enter Low: ";
+    cin >> array[month][1];
+    cout << "\n";
   }
 }
 
-double averageHigh(double array[][months]) {
-  double avgHigh;
-
-  for (int c = 0; c < months; c++) {
-    avgHigh += array[0][c];
-  }
-  return avgHigh / months;
+void printMonth(int index) {
+  cout << MONTH_NAMES[index] << ":\n\n";
 }
 
-double averageLow(double array[][months]) {
-  double avgLow;
+double averageHigh(double array[TOTAL_MONTHS][2]) {
+  double sum;
 
-  for (int c = 0; c < months; c++) {
-    avgLow += array[1][c];
+  for (int month = 0; month < TOTAL_MONTHS; month++) {
+    sum += array[month][0];
   }
-  return avgLow / months;
+
+  return sum / TOTAL_MONTHS;
 }
 
-int indexHighTemp(double array[][months]) {
-  int temp;
+double averageLow(double array[TOTAL_MONTHS][2]) {
+  double sum;
 
-  for (int c = 0; c < months; c++) {
-      temp = (temp < array[0][c]) ? temp : array[0][c];
+  for (int month = 0; month < TOTAL_MONTHS; month++) {
+    sum += array[month][1];
   }
-  return temp;
+
+  return sum / TOTAL_MONTHS;
 }
 
-int indexLowTemp(double array[][months]) {
-  int temp;
+int indexHighTemp(double array[TOTAL_MONTHS][2]) {
+  int highest;
 
-  for (int c = 0; c < months; c++) {
-      temp = (temp < array[1][c]) ? temp : array[1][c];
+  for (int month = 0; month < TOTAL_MONTHS; month++) {
+      if (array[month][0] > highest) {
+        highest = array[month][0];
+      }
   }
-  return temp;
+  return highest;
+}
+
+int indexLowTemp(double array[TOTAL_MONTHS][2]) {
+  int lowest;
+
+  for (int month = 0; month < TOTAL_MONTHS; month++) {
+      if (array[month][1] < lowest) {
+        lowest = array[month][1];
+      }
+  }
+  return lowest;
 }
